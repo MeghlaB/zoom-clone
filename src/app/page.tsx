0131df30df0abcc419dbb0a5bd733c530eb26649
MeghlaB <meghlab964@gmail.com>
2025-07-31 19:45:55 +0600

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-     <Button>Click me</Button>
+     <Button>Click Me On</Button>
     </>
   );
 }
